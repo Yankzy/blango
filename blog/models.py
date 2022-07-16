b7cde0fb61b1c505ae11b4813354446df4ab9cea
Kuyateh Yankz <qubzen@gmail.com>
2022-07-16 17:41:02 +0000

    Finish generic relationships

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
@@ -18,6 +21,35 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation('Comment')
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    modified_at = models.DateTimeField(auto_now=True, default=timezone.now)
+
+
+# In [1]: from django.contrib.contenttypes.models import ContentType
+
+# In [2]: from blog.models import Post, Comment
+  
+# In [3]: post_type = ContentType.objects.get_for_model(Post)
+  
+# In [4]: p = Post.objects.first()
+  
+# In [5]: c = Comment.objects.filter(content_type=post_type, object_id=p.pk)
+  
+# In [6]: c
+# Out[6]: <QuerySet [<Comment: Comment object (1)>, <Comment: Comment object (2)>]>
+
+# p = Post.objects.first()
+# p.comments.all()
+# c1 = p.comments.all()[0]
+# p.comments.remove(c1)
