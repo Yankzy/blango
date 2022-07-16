ddc3d1bce62c1abb4bcecd660f7c6b36e76e14c5
Kuyateh Yankz <qubzen@gmail.com>
2022-07-16 20:35:09 +0000

    Finish html frameworks

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-
+from django.utils import timezone
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
@@ -13,7 +13,7 @@ class Tag(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(blank=True, null=True)
     title = models.TextField(max_length=100)
@@ -32,8 +32,8 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
-    modified_at = models.DateTimeField(auto_now=True, default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    modified_at = models.DateTimeField(default=timezone.now)
 
 
 # In [1]: from django.contrib.contenttypes.models import ContentType
